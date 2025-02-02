497f75d0e419d48aa0e472f9c5202f69b71ae650
Palm Civet <palmcivet_email@qq.com>
2025-02-02 18:03:18 +0800

    feat: update environment configuration and add env.example

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,32 @@
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tailwind from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+import { name } from './package.json';
 
-export default defineConfig({
-  root: 'src',
-  base: '/arknights-scoreboard/',
-  envDir: path.resolve(__dirname, './'),
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@rules': path.resolve(__dirname, './rules'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    root: 'src',
+    base: env.DEPLOY_AS_SUB_FOLDER === 'true' ? `/${name}/` : '/',
+    envDir: path.resolve(__dirname, './'),
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@rules': path.resolve(__dirname, './rules'),
+      },
     },
-  },
-  build: {
-    outDir: path.resolve(__dirname, './dist'),
-    emptyOutDir: true,
-  },
-  css: {
-    postcss: {
-      plugins: [tailwind(), autoprefixer()],
+    build: {
+      outDir: path.resolve(__dirname, './dist'),
+      emptyOutDir: true,
     },
-  },
-  plugins: [vue()],
+    css: {
+      postcss: {
+        plugins: [tailwind(), autoprefixer()],
+      },
+    },
+    plugins: [vue()],
+  };
 });
